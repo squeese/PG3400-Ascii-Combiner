7385498588d140887608107bc20f2a02431b35f7
Lennart Hansen <lennart@hans1.no>
2018-10-14 20:48:39 +0200

    ok

--- parser.c
+++ parser.c
@@ -18,26 +18,69 @@ int p_parse(char* path, void* out) {
     return -1;
   }
 
-  if (out != NULL) {
-    // assign the user specified name, so p_scan_filenames wont parse out
-    // the name from the filenames
-    p.name = (char*)out;
-  }
-
   int r_signal = 0;
   if ((r_signal = p_scan_filenames(&p, d_pointer)) == 0) {
     printf("> X:%d Y:%d name:%s\n", p.x, p.y, p.name);
 
-    FILE* f_out = fopen(p.name, "w+");
-    if (f_out == NULL) {
-      printf("Unable to open file '%s'\n", p.name);
-      perror("Error: ");
-      return -1;
+    // File pointer for writing the resulted knitted ascii artsci partsci
+    FILE* f_out_ptr;
+    if (out != NULL) {
+      // use the output filename specified by the user from commandline
+      f_out_ptr = fopen((char*)out, "w+");
+    } else {
+      // fallback on the default name provided by the filenames
+      char name[strlen(p.name) + 4];
+      sprintf(name, "%s.txt", p.name);
+      f_out_ptr = fopen(name, "w+");
     }
 
-    fclose(f_out);
-  }
+    // BUG,fasldkfjaslkfj
+    if (f_out_ptr == NULL) {
+      printf("Unable to open file '%s'\n", p.name);
+      perror("Error: ");
+      r_signal = -1;
+    } else {
 
+      // create a filename buffer for creating pathnames to each file
+      int f_size = strlen(path) + log10(p.x) + log10(p.y) + strlen(p.name) + 16;
+      char f_path[f_size];
+
+      // int row_size = 30 * (p.x + 1);
+      // char* f_buffer = malloc(sizeof(char) * row_size);
+      // char f_buffer[row_size + 2];
+      char f_buffer[30];
+      // f_buffer[30] = '\r';
+      // f_buffer[31] = '\n';
+
+      FILE** f_in_ptr = malloc(sizeof(FILE*) * (p.x + 1));
+      int x, y, s;
+      for (y = 0; y <= p.y; y++) {
+        // open read pointers for column
+        for (x = 0; x <= p.x; x++) {
+          snprintf(f_path, f_size, "%s/part_%d_%d_%s.txt", path, x, y, p.name);
+          printf("open: [%d] (%d) '%s'\n", x, f_size, f_path);
+          f_in_ptr[x] = fopen(f_path, "r");
+        }
+
+        // read stuff
+        for (s = 0; s < 30; s++) {
+          for (x = 0; x <= p.x; x++) {
+            fread(&f_buffer, sizeof(char), 30, f_in_ptr[x]);
+            fwrite(&f_buffer, sizeof(char), 30, f_out_ptr);
+          }
+          fputc('\r', f_out_ptr);
+          fputc('\n', f_out_ptr);
+        }
+
+        // close column pointers
+        for (x = 0; x <= p.x; x++) {
+          printf("close: [%d] (%d) '%s'\n", x, f_size, f_path);
+          fclose(f_in_ptr[x]);
+        }
+      }
+      fclose(f_out_ptr);
+    }
+  }
   closedir(d_pointer);
 
   // cleanup anything stored in the heap
@@ -65,6 +108,7 @@ int p_scan_filenames(struct Parser* p, DIR* d_pointer) {
 
     // find the position of the X value in the filename
     // only need to do this once, since we assume all files are formatted equally
+    /*
     if (x_ptr == NULL) {
       for (int i = 1;; i++) {
         if (i >= f_length) {
@@ -77,6 +121,9 @@ int p_scan_filenames(struct Parser* p, DIR* d_pointer) {
         }
       }
     }
+    */
+
+    x_ptr = f_entry->d_name + 5;
 
     // converting the X and Y values to int
     // storing the max value of each of them, since we only need to know the depth
@@ -85,19 +132,19 @@ int p_scan_filenames(struct Parser* p, DIR* d_pointer) {
     // x_ptr points the the position in the filename where the X value starts
     // when reading the X value with strtol, we update the y_ptr to the position after X ends
     // we also move the y_ptr to the end of the y value, to grabe the name (which comes after the Y value)
-    p->y = (int)fmaxl((long)p->y, strtol(y_ptr + 1, &y_ptr, 10));
+    p->y = (int)fmaxl((long)p->y, strtol(y_ptr + 1, NULL, 10));
 
     // if the user didnt pass along a name for the resulting output file
     // we scan and use the name on the file
-    if (p->name == NULL) {
+    if (p->name == NULL && 0) {
       // figuring out the length of the name portion of filename;
       // given the total length of the filename, and the y_ptr position
-      int n_length = f_length - (int)(y_ptr - f_entry->d_name);
+      int n_length = f_length - (int)(y_ptr - f_entry->d_name) - 5;
       p->name = malloc(sizeof(char) * n_length);
       memcpy(p->name, y_ptr + 1, n_length);
     }
 
-    printf("> %p %c (%d, %d) %s\n", x_ptr, *x_ptr, p->x, p->y, f_entry->d_name);
+    printf("> %p %c (%d, %d) %s\n", y_ptr, *(y_ptr + 1), p->x, p->y, f_entry->d_name);
   }
   return 0;
 }
--- view.c
+++ view.c
@@ -1,45 +1,13 @@
 #include "parser.h"
 #include <stdio.h>
-// #include <string.h>
-// #include <math.h>
-// #include <inttypes.h>
-// #include <stdlib.h>
-
 
 int main(int count, char* args[]) {
-  printf("args: %d\n", count);
-  for (int i = 0; i < count; i++) {
-    printf("%d: %s\n", i, args[i]);
-  }
-
   if (count < 2) {
-    printf("> Error: Must provide folder name of ascii art to be processed");
-    printf("> view [ mickey | wolf ]\n");
+    printf("\nError: Must provide path to the folder with ascii art to be processed\n");
+    printf("  view PATH_FOLDER [ OUT_FILENAME ]\n\n");
+    printf("Examples:\n  view ./mickey bigmickey.txt\n  view ./wolf\n\n");
     return -1;
   }
-
-  return p_parse("./mickey", NULL);
-
-  // if (test("./wolf/part_0_0_wolf.txt") != 0) return -1;
-  // if (test("./wolf/part_0_1_wolf.txt") != 0) return -1;
-
-  /*
-  int result;
-  struct Parser p = {};
-  if ((result = p_initialize(&p, "./mickey")) == 0) {
-    result = p_write(&p, NULL);
-  }
-  p_free(&p);
-  return result;
-  */
-
-  /*
-  char dirLength = strlen(args[1]);
-  char dirName[dirLength+ 3];
-  dirName[0] = '.';
-  dirName[1] = '/';
-  memcpy(&dirName[2], args[1], dirLength);
-  dirName[dirLength + 2] = '\0';
-  printf("'%s'\n", dirName);
-  */
+  printf("\n view %s %s\n\n", args[1], args[2]);
+  return p_parse("./wolf", "out.txt");
 }
